88b5d2d4b1ec7e367e8bcb292cb3bd52bb499589
NicMagnier <nmagnier@gmail.com>
2023-06-26 21:52:48 +0200

    tweaks
    change camel case to snake case\
    added option to get_custom_data to get a field directly.

--- LDtk.lua
+++ LDtk.lua
@@ -261,8 +261,8 @@ function LDtk.load_level( level_name )
 		
 	-- load level's custom fields
 	for index, field_data in ipairs(level_data.fieldInstances) do
-		level.customData = {}
-		level.customData[ field_data.__identifier ] = field_data.__value
+		level.custom_data = {}
+		level.custom_data[ field_data.__identifier ] = field_data.__value
 	end
 
 	-- handle layers
@@ -426,7 +426,7 @@ function LDtk.get_entities( level_name, layer_name )
 end
 
 -- return a tilemap for the level
--- @layer_name is optional, if nil than will return the first layer with tiles
+-- @layer_name is optional, if nil then will return the first layer with tiles
 function LDtk.create_tilemap( level_name, layer_name )
 	local layer = _.get_tile_layer( level_name, layer_name )
 	if not layer then return end
@@ -454,13 +454,17 @@ end
 -- return the position and site of the level in the world
 -- always available, the level doesn't need to be loaded
 function LDtk.get_rect( level_name )
-	return _level_rects[level_name]
+	return _level_rects[ level_name ]
 end
 
 -- return custom data for the specified level 
--- LDtk.get_customData( "Level_0" )
-function LDtk.get_customData( level_name )
-	return _levels[level_name].customData 
+-- @field_name is optional, if nil then it will return all the fields as a table
+function LDtk.get_custom_data( level_name, field_name )
+	if field_name then
+		return _levels[ level_name ].custom_data[ field_name ]
+	end
+
+	return _levels[ level_name ].custom_data
 end
 
 -- return all the tileIDs tagged in LDtk with tileset_enum_value
